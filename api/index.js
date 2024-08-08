7480aea965989d81a117a6b5d32bb6ab0469b1b6
Praharsh <praharsh_bc22070@srmcem.ac.in>
2024-08-08 13:22:58 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(process.env.mongo).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error:', error);
+});
 
 const app = express();
 
